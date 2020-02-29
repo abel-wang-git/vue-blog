704e9a2a4833aacb069945105234a860b50f748b
wanghuiwen <wanghuiwen312@sina.com>
2020-02-29 17:45:50 +0800

    fix bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -73,9 +73,8 @@ service.interceptors.response.use(
     }
   },
   error => {
-    console.log('err' + error) // for debug
     Message({
-      message: error.message,
+      message: error.response.data.message,
       type: 'error',
       duration: 5 * 1000
     })
--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,6 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: true,
     overlay: {
       warnings: false,
       errors: true
